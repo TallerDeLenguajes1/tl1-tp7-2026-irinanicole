2e2d07bde615306d736c491b13a6273197fb8897
Nicky Bourdette (Hp) <inbourdette.est@gmail.com>
2026-05-26 20:11:48 -0300

    ejercicio 1 - Proyecto Calculadora: check

---
+++ Calculadora/Program.cs
@@ -0,0 +1,115 @@
+﻿using EspacioCalculadora; //nombre del espacio donde se encuentra la clase
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("--- INICIO ---\n");
+Console.ResetColor();
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("Ingrese un numero: ");
+Console.ResetColor();
+double dato;
+string? s = Console.ReadLine();
+bool esOperable = double.TryParse(s, out dato);
+
+if (esOperable)
+{
+    Calculadora numero = new Calculadora(dato);
+    int seguir;
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine("¿Desea realizar una operación? SI(x)|NO(0) ");
+    Console.ResetColor();
+    s = Console.ReadLine();
+    esOperable = int.TryParse(s, out seguir);
+    bool estaLimpia = false;
+    while (esOperable && seguir != 0)
+    {
+        if (estaLimpia)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Ingrese un NUEVO valor inicial:");
+            Console.ResetColor();
+            s = Console.ReadLine();
+            if (double.TryParse(s, out dato))
+            {
+                numero = new Calculadora(dato);
+            }
+        }
+        double termino, num_anterior;
+        num_anterior = numero.GetResultado();
+        int operacion;
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine("Selecciona una operación: ");
+        Console.ResetColor();
+        Console.WriteLine("(1): SUMA");
+        Console.WriteLine("(2): RESTA");
+        Console.WriteLine("(3): PRODUCTO");
+        Console.WriteLine("(4): DIVISION");
+        Console.WriteLine("(0): LIMPIAR");
+        s = Console.ReadLine();
+        esOperable = int.TryParse(s, out operacion);
+        if (operacion >= 1 && operacion <=4)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Ingrese el segundo termino: ");
+            Console.ResetColor();
+            s = Console.ReadLine();
+            esOperable = double.TryParse(s, out termino);
+            if(esOperable)
+            {
+                switch (operacion)
+                {
+                    case 1:
+                        numero.Sumar(termino);
+                        Console.WriteLine($"{num_anterior} + {termino} = {numero.GetResultado()}");
+                        break;
+                    case 2:
+                        numero.Restar(termino);
+                        Console.WriteLine($"{num_anterior} - {termino} = {numero.GetResultado()}");
+                        break;
+                    case 3:
+                        numero.Multiplicar(termino);
+                        Console.WriteLine($"{num_anterior} * {termino} = {numero.GetResultado()}");
+                        break;
+                    case 4:
+                        numero.Dividir(termino);
+                        Console.WriteLine($"{num_anterior} / {termino} = {numero.GetResultado()}");
+                        break;
+                    default:
+                        break;
+                }
+            }
+        } else if (operacion == 0) {
+            numero.Limpiar();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("--> Calculadora REINICIADA <--");
+            Console.WriteLine("------------------------------\n");
+            Console.ResetColor();
+            estaLimpia = true;
+
+        } else {
+            // Cambia el color del texto a rojo para indicar un error
+            Console.ForegroundColor = ConsoleColor.Red;
+            // Muestra el mensaje de error
+            Console.WriteLine("Error: La opcion ingresada es inválida.");
+            // Restaura el color original de la consola
+            Console.ResetColor();
+        }
+
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("¿Seguir operando? SI(x)|NO(0) ");
+        Console.ResetColor();
+        s = Console.ReadLine();
+        esOperable = int.TryParse(s, out seguir);
+    }
+
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Error: el dato ingresado es inválido para operar.");
+    Console.ResetColor();
+}
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("\n--- Fin del Programa ---");
+Console.ResetColor();
